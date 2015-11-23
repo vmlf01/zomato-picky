3eb0c695dd5dd9c055840844d14922b544fce60a
Vitor Fernandes <vmlf01@gmail.com>
2015-11-23 01:33:36 +0000

    feat(api): return restaurant random pictures

--- modules/visual_search/zomato_client.js
+++ modules/visual_search/zomato_client.js
@@ -97,18 +97,51 @@ var processBestRatedResponse = function (response, cb) {
 };
 
 var getZomatoRestaurantImages = function (restaurant, cb) {
+// zomato restaurant api only provides photos to partners
+// so get pictures from site backend page
+// https://www.zomato.com/php/load_more_res_pics.php
+    // res_id = restaurant.id
+    //
+    /*
+     {
+     "html": "\n                    <a href='#' draggable='true' style='background-image: none;' class='photo-inner js-heart-container' data-photo_id='r_ODIzMjEwOTQxOT'><span class='photo-like-icon js-fp-like no-events' data-icon='f'></span><img class='res-photo-thumbnail thumb-load lazy' data-type='res' data-photo_id='r_ODIzMjEwOTQxOT'  data-index='1' data-original='https://c.zmtcdn.com/data/pictures/2/8201122/afe22753fb243b15a0894556e1fdbf06_200_thumb.jpg' src='https://d.zmtcdn.com/images/photoback.png' /></a>\n
+     }
+     */
 
-    var params = {
-        url: url.resolve(config.zomato.apiUrl, 'restaurant'),
-        qs: {
-            res_id: restaurant.id
-        }
+    var reqParams = {
+        url: 'https://www.zomato.com/php/load_more_res_pics.php',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'res_id=' + restaurant.id
     };
 
+    request.post(reqParams, function (err, response, body) {
+        if (err) {
+            cb(err);
+        }
+        else {
+            var photos = [];
+            var match;
+            var photosRegex = / src='(.*?)'/g;
+
+            while((match = photosRegex.exec(body)) !== null) {
+                photos.push(match[1]);
+            }
+            console.log(photos);
+
+            var randomIndex = getRandomInt(0, photos.length);
+            var photo = photos[randomIndex].replace(/\\/g, '');
+
+            cb(null, photo);
+        }
+    });
+/*
     async.waterfall([
         async.apply(executeZomatoRequest, params),
         processImagesResponse
     ], cb);
+    */
 };
 
 var processImagesResponse = function (restaurant, cb) {
